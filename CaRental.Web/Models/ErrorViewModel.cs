614d0e32a541efaf40251a77c31057d13e3117c2
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2022-02-17 21:22:41 +0100

    Initial commit

---
+++ CaRental.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CaRental.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
